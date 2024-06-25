595c20324564f25701845247f3e2f271f7b5baa1
BULLETBOT <csgodenisz@gmail.com>
2024-06-25 03:23:45 +0200

    Street Zombies only spawn at Seed Chooser

--- Lawn/CutScene.cpp
+++ Lawn/CutScene.cpp
@@ -491,7 +491,7 @@ void CutScene::PlaceStreetZombies()
 
 	mPlacedZombies = true;
 
-	if (mApp->IsFinalBossLevel() || !mBoard->ChooseSeedsOnCurrentLevel())
+	if (!mBoard->ChooseSeedsOnCurrentLevel())
 		return;
 
 	// 以下统计出怪列表中各种可预览的僵尸的数量
