c3e8e76d269df191e2324679da9a40067f159ff5
BULLETBOT <csgodenisz@gmail.com>
2024-07-03 05:03:45 +0200

    More Comment Cleanups

--- Lawn/Board.cpp
+++ Lawn/Board.cpp
@@ -647,8 +647,6 @@ void Board::PickZombieWaves()
 				continue;
 		}
 
-		// ------------------------------------------------------------------------------------------------
-		// ------------------------------------------------------------------------------------------------
 		int& aZombiePoints = aZombiePicker.mZombiePoints;
 		if (mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_LAST_STAND)
 		{
@@ -703,8 +701,6 @@ void Board::PickZombieWaves()
 			aZombiePoints *= 2;
 		}
 		
-		// ------------------------------------------------------------------------------------------------
-		// ------------------------------------------------------------------------------------------------
 		if (aIntroZombieType != ZombieType::ZOMBIE_INVALID && aIntroZombieType != ZombieType::ZOMBIE_DUCKY_TUBE)
 		{
 			bool aSpawnIntro = false;
@@ -775,8 +771,6 @@ void Board::PickZombieWaves()
 			}
 		}
 		
-		// ------------------------------------------------------------------------------------------------
-		// ------------------------------------------------------------------------------------------------
 		while (aZombiePoints > 0 && aZombiePicker.mZombieCount < MAX_ZOMBIES_IN_WAVE)
 		{
 			ZombieType aZombieType = PickZombieType(aZombiePoints, aWave, &aZombiePicker);
