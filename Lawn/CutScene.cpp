ae9daa84eda39ed8924a30162d518e22025f26d9
BULLETBOT <csgodenisz@gmail.com>
2024-07-05 19:19:24 +0200

    Fixed UI Layering

--- Lawn/CutScene.cpp
+++ Lawn/CutScene.cpp
@@ -791,7 +791,7 @@ void CutScene::StartLevelIntro()
 	{
 		mCrazyDaveDialogStart = mApp->IsFirstTimeAdventureMode() ? 1301 : 1304;
 	}
-	else if (!mApp->IsFirstTimeAdventureMode() && aLevel == 1 && !mApp->mPlayedQuickplay)
+	else if (!mApp->IsFirstTimeAdventureMode() && aLevel == 1 && (!mApp->mPlayedQuickplay || (mApp->mPlayedQuickplay && mApp->mCrazySeeds)))
 	{
 		mCrazyDaveDialogStart = 1601;
 	}
@@ -2284,4 +2284,9 @@ bool CutScene::IsAfterSeedChooser()
 bool CutScene::ShowZombieWalking()
 {
 	return mCutsceneTime > LostTimePanRightStart;
+}
+
+bool CutScene::IsAfterPanLeft()
+{
+	return mCutsceneTime <= TimePanLeftEnd + mCrazyDaveTime;
 }
