8a48568caa9055ff7c49fe98a4ae4c012beca657
BULLETBOT <csgodenisz@gmail.com>
2024-07-06 18:46:02 +0200

    Renamed method

--- Lawn/CutScene.cpp
+++ Lawn/CutScene.cpp
@@ -2286,7 +2286,7 @@ bool CutScene::ShowZombieWalking()
 	return mCutsceneTime > LostTimePanRightStart;
 }
 
-bool CutScene::IsAfterPanLeft()
+bool CutScene::IsPanningLeft()
 {
 	return mCutsceneTime <= TimePanLeftEnd + mCrazyDaveTime;
 }
