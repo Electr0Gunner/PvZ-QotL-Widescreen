a96ee1c860afd545290e4bcd2dd7a94ce3303ecb
BULLETBOT <csgodenisz@gmail.com>
2024-07-13 02:03:38 +0200

    Update Zombie.cpp

--- Lawn/Zombie.cpp
+++ Lawn/Zombie.cpp
@@ -3563,11 +3563,11 @@ void Zombie::SetupReanimForLostArm(unsigned int theDamageFlags)
             break;
         }
         case ZombieType::ZOMBIE_DANCER:
-            GetTrackPosition("Zombie_outerarm_lower", aPosX, aPosY);
+            GetTrackPosition("Zombie_disco_outerarm_lower", aPosX, aPosY);
             aBodyReanim->SetImageOverride("Zombie_disco_outerarm_upper", IMAGE_REANIM_ZOMBIE_DISCO_OUTERARM_UPPER2);
             break;
         case ZombieType::ZOMBIE_BACKUP_DANCER:
-            GetTrackPosition("Zombie_outerarm_lower", aPosX, aPosY);
+            GetTrackPosition("Zombie_disco_outerarm_lower", aPosX, aPosY);
             aBodyReanim->SetImageOverride("Zombie_disco_outerarm_upper", IMAGE_REANIM_ZOMBIE_BACKUP_OUTERARM_UPPER2);
             break;
         case ZombieType::ZOMBIE_LADDER:
